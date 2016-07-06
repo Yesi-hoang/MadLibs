556f2fff8e54762993e314407baa50f59d1f5c7c
Yesi <317447721@qq.com>
2016-07-06 19:53:13 +0800

    Initial Commit

---
+++ MadLibs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MadLibs
+//
+//  Created by 黄丽萍 on 16/7/6.
+//  Copyright © 2016年 黄丽萍. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by 黄丽萍 on 16/7/6.
+//  Copyright © 2016年 黄丽萍. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
